b6ba96c06ed8f7edbc4a2021d41c1a1d06abeb84
윤준식 <justbeber@naver.com>
2024-08-09 13:11:32 +0900

    Test/#3: 비밀번호 암호화 회원가입 테스트

--- src/main/java/com/example/board/application/dto/request/CreateAccountRequest.java
+++ src/main/java/com/example/board/application/dto/request/CreateAccountRequest.java
@@ -3,12 +3,14 @@ package com.example.board.application.dto.request;
 import com.example.board.domain.member.entity.Gender;
 import com.example.board.domain.member.entity.Member;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class CreateAccountRequest {
     private String memberId;
     private  String password;
@@ -19,10 +21,9 @@ public class CreateAccountRequest {
     private String email;
 
     public Member toMember(){
-        this.password= BCrypt.hashpw(this.password,BCrypt.gensalt());
         return new Member(
                 memberId,
-                password,
+                BCrypt.hashpw(this.password,BCrypt.gensalt()),
                 name,
                 call,
                 gender,
