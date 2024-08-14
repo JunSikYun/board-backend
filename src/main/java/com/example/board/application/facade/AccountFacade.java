87628e06965df50264e6766dd49f4fd537069ae7
윤준식 <justbeber@naver.com>
2024-08-14 11:57:31 +0900

    Feat/#5: Jwt 기반 로그인 구현

--- src/main/java/com/example/board/application/facade/AccountFacade.java
+++ src/main/java/com/example/board/application/facade/AccountFacade.java
@@ -4,15 +4,18 @@ import com.example.board.application.dto.request.CreateAccountRequest;
 import com.example.board.application.dto.response.CanUseMemberIdResponse;
 import com.example.board.domain.member.entity.Member;
 import com.example.board.domain.member.service.MemberService;
+import com.example.board.global.jwt.JwtProvider;
 import jakarta.persistence.Access;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class AccountFacade {
     private final MemberService memberService;
+    private final JwtProvider jwtProvider;
     public CanUseMemberIdResponse canUseMemberId(String memberId){
         boolean status=memberService.notExistsByMemberId(memberId);
         return new CanUseMemberIdResponse(status);
@@ -34,6 +37,27 @@ public class AccountFacade {
         }
         Member member=createAccountRequest.toMember();
         memberService.save(member);
+    }
 
+
+    public String loginAccount(String memberId, String password){
+        // 1.memberId 조회
+        // 2.가져온 값에서 password와 일치하나 확인
+
+        //memberId 조회
+        Member member=memberService.findByMemberId(memberId);
+
+        String databasePassword=member.getPassword();
+        if(!BCrypt.checkpw(password,databasePassword)){
+            throw new RuntimeException();
+        }
+        //여기서 jwt 발급 필요
+
+        //DB에서 가져오는 PW는 암호화되어있기 때문에 false만 뜸
+//        if(databasePassword.equals(password)){
+//            return true;
+//        }
+//        return false;
+        return jwtProvider.createAccessToken(member.getId());
     }
 }
